c16f4ece6fbe45d93be6a5992f5c146b0141ae30
YoussefElAraby <arabymohammedyoussef123@gmail.com>
2026-01-05 19:35:42 +0200

    Use Express app directly on Vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,3 @@
-const serverless = require('serverless-http');
 const app = require('../app');
 
-module.exports = async (req, res) => {
-  const handler = serverless(app);
-  return handler(req, res);
-};
+module.exports = app;
